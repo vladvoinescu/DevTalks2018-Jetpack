191b93cdc33a3a76583202e679345c900e235a06
Vlad <vlad.voinescu@db.com>
2018-06-11 12:56:12 +0300

    first commit; known and to be fixed issues: crash (sometimes NPE when opening app?), rating icons quality++, add app icon, delete from db when un-favorited, search results paging

---
+++ app/src/main/java/com/db/devtalks/Utils.java
@@ -0,0 +1,15 @@
+package com.db.devtalks;
+
+/**
+ * TODO: class description.
+ * <p>
+ * Copyright © 2017 Deutsche Bank. All rights reserved.
+ */
+
+class Utils {
+
+    void doSomeSecretStuff() {
+        boolean thisShouldBeEncrypted = true;
+    }
+
+}
